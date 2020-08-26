97635e06a352014055e2b4abf0e6265c4ed6a47e
duqiuyi <512608603@qq.com>
2020-08-26 17:38:55 +0800

    配置登录和注册的路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// 导入组件
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  { path: '/login', component: Login },
+  { path: '/register', component: Register }
+]
 
 const router = new VueRouter({
   routes

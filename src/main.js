ad44d491100fbcf76b57ba1ad92262abe35cdd7d
duqiuyi <512608603@qq.com>
2020-08-26 19:50:40 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入路由
 import router from './router'
 
+// 导入通用样式
+import './styles/base.less'
+import './styles/iconfont.css'
+
+// 导入vant
+// 导入所有组件(方便)
+// import vant from 'vant'
+// import 'vant/lib/index.css'
+
+// Vue.use(vant)
+
+// 自动按需引入组件(轻量)
+import 'vant/lib/index.css'
+import { Button } from 'vant'
+Vue.use(Button)
+
+// 清除vue自带的提示信息
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>登录组件</div>
+  <div>
+    登录组件
+    <div class="fonts iconfont iconfenxiang"></div>
+    <div class="fonts iconfont iconnew"></div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
@@ -7,4 +16,7 @@ export default {}
 </script>
 
 <style>
+.fonts {
+  background-color: red;
+}
 </style>
